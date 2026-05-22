7f9fee22b001f464aaf21b3d495cc16042c325a8
w2599 <114452622+w2599@users.noreply.github.com>
2026-05-22 15:53:34 +0800

    roothide_blacklist

---
+++ .roothide.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd $(dirname $0)/src;pwd
+
+
+export package FINALPACKAGE=1
+export THEOS_PACKAGE_SCHEME=roothide
+
+
+if [ $1 -eq "0" ]
+then
+	make do
+	make clean
+	exit
+fi
+
+
+make package
+mv ./packages/*.deb ~/Documents/GitHub/myTweaks/roothide/
+make clean
+# mv ./packages/*.deb ~/Desktop/tweaks/debs/ && make clean
+
+
+# clean::
+	# rm -rf .theos/obj
+	# rm -rf packages
