b87780ab984005ac02e0eb472ba7b314519e4cad
w2599 <114452622+w2599@users.noreply.github.com>
2026-08-13 15:19:53 +0800

    排除系统 app 和 plugins

--- src/GridCell.m
+++ src/GridCell.m
@@ -65,7 +65,15 @@ static bool psIsUserApp(PSProc *proc)
 {
 	if (!proc.app || !proc.executable)
 		return false;
-	return [proc.executable rangeOfString:@"/Application/"].location != NSNotFound;
+	
+	NSString *executable = proc.executable;
+	BOOL isContainersBundleApplication = [executable containsString:@"/var/containers/Bundle/Application/"];
+	BOOL isAppPlugins = [executable containsString:@"/var/containers/Bundle/Application/"] && [executable containsString:@"/PlugIns/"];
+	
+	if (isContainersBundleApplication && !isAppPlugins) {
+		return true;
+	}
+	return false;
 }
 
 
@@ -164,6 +172,8 @@ static bool psIsUserApp(PSProc *proc)
 		self.textLabel.textColor = [UIColor colorWithRed:0.12 green:0.5 blue:0.12 alpha:1.0];
 	} else if (proc.uid == 0) {
 		self.textLabel.textColor = [UIColor colorWithRed:0.55 green:0.55 blue:1.0 alpha:1.0];
+	}else if(@available(iOS 13, *)) {
+		self.textLabel.textColor = [UIColor labelColor];
 	}
 	self.detailTextLabel.text = [proc.executable stringByAppendingString:proc.args];
 	if (proc.icon)
